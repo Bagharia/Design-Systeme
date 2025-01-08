cda6ed98b5e2ac1168e67d4653146e522d0c7dfb
Bagharia <hu.alexis@hotmail.com>
2025-01-08 16:18:56 +0100

    up

--- src/stories/Input.stories.jsx
+++ src/stories/Input.stories.jsx
@@ -10,16 +10,8 @@ export default {
 
 // Création des différentes stories pour les cas d'usage
 export const Default = () => <Input placeholder="Enter text" />;
-export const Hover = () => <Input placeholder="Hover input"  />;
-export const Focus = () => {
-  const inputRef = React.useRef(null);
-  React.useEffect(() => {
-    inputRef.current?.focus();
-  }, []);
-  return <Input ref={inputRef} placeholder="Focus state input" />;
-};
-export const Disabled = () => <Input placeholder="Disabled input" disabled />;
-export const Error = () => <Input placeholder="Enter text" error="This field is required" />;
+export const Hover = () => <Input className="input--hover" placeholder="Hover input"  />;
+export const Required = () => <Input className="input--required"placeholder="Enter text" />;
 export const Large = () => <Input className="input--large" placeholder='Large input' />;
 export const Medium = () => <Input className="input--medium" placeholder='Medium input' />;
 export const Small = () => <Input className="input--small" placeholder='Small input' />;
